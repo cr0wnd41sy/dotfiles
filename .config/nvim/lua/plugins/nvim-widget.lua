071fc1cc33f58fff603499fffd0167e7268311f5
cr0wnd41sy <cr0wnd41sy@proton.me>
2025-04-01 22:38:59 +0800

    first commit

---
+++ .config/nvim/lua/plugins/nvim-widget.lua
@@ -0,0 +1,28 @@
+return {
+	-- nvim tree file manager
+	--{
+	--	'nvim-tree/nvim-tree.lua',
+	--	dependencies = {
+	--		'nvim-tree/nvim-web-devicons'
+	--	},
+	--	config = function()
+	--		require("nvim-tree").setup{}
+	--	end,
+	--},
+	-- nvim terminal
+	--{
+	--	"s1n7ax/nvim-terminal",
+	--	    config = function()
+	--		    vim.o.hidden = true
+	--		    require('nvim-terminal').setup()
+	--	    end,
+	--},
+	
+	-- bufferline
+	--{
+	--	'akinsho/buferline.nvim',
+	--	dependencies = {
+	--		'nvim-tree/nvim-web-devicons'
+	--	},
+	--}
+}
